5cb6eb57d771aeb738ee16afa48a89aa06e667fc
Leonardo Marinho <leonardo.marinho@gmail.com>
2020-05-15 18:00:33 -0300

    Adds POST endpoint sample

--- src/main/java/com/marinho/springbootboilerplate/controller/HelloWorldController.java
+++ src/main/java/com/marinho/springbootboilerplate/controller/HelloWorldController.java
@@ -1,17 +1,28 @@
 package com.marinho.springbootboilerplate.controller;
 
 import com.marinho.springbootboilerplate.model.Message;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/hello-world")
 public class HelloWorldController {
 
     private static final String HELLO_WORLD_MESSAGE = "Hello, World!";
 
-    @GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE })
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Message message(){
-        return new Message(HELLO_WORLD_MESSAGE);
+        var createdMessage = new Message();
+        createdMessage.setMessage(HELLO_WORLD_MESSAGE);
+        return createdMessage;
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Message> somePostMethod(@RequestBody Message message) {
+        var createdMessage = new Message();
+        createdMessage.setMessage("HEY, you posted ".concat(message.getMessage()));
+        return new ResponseEntity<Message>(createdMessage, HttpStatus.OK);
     }
 }
--- src/main/java/com/marinho/springbootboilerplate/model/Message.java
+++ src/main/java/com/marinho/springbootboilerplate/model/Message.java
@@ -3,14 +3,7 @@ package com.marinho.springbootboilerplate.model;
 public class Message {
     private String message;
 
-    public Message(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
+    public String getMessage() { return message; }
     public void setMessage(String message) {
         this.message = message;
     }
